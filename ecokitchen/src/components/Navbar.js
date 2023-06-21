0491d20a6916d5d6ee21a5f5783d68fa022f1172
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-06-21 13:47:38 +0100

    navbar got bootstrap

--- ecokitchen/src/components/Navbar.js
+++ ecokitchen/src/components/Navbar.js
@@ -4,57 +4,47 @@ import { BiListCheck } from "react-icons/bi";
 import { BiBarChartAlt } from "react-icons/bi";
 import { BiBookOpen } from "react-icons/bi";
 
-import {Link, useLocation } from "react-router-dom"
-
-import React from 'react';
-
 const BottomNavbar = () => {
-
   return (
-    <footer className="navbar navbar-expand-lg navbar-light bg-light fixed-bottom">
-      <div className="container">
-        <div className="row justify-content-center">
-
-          <div className="col text-center">
-            <div className="navbar-icon">
-              <BiHomeAlt className="navbar-icon-image" />
-              <span className="navbar-icon-name">Home</span>
-            </div>
+    <footer className="navbar navbar-expand-lg navbar-light bg-light fixed-bottom border border-danger">
+      <div className="container d-flex justify-content-between">
+        <div className="text-center">
+          <div className="navbar-icon">
+            <BiHomeAlt className="navbar-icon-image" />
+            <span className="navbar-icon-name">Home</span>
           </div>
+        </div>
 
-          <div className="col text-center">
-            <div className="navbar-icon">
-              <BiFridge className="navbar-icon-image" />
-              <span className="navbar-icon-name">Food</span>
-            </div>
+        <div className="text-center">
+          <div className="navbar-icon">
+            <BiFridge className="navbar-icon-image" />
+            <span className="navbar-icon-name">Food</span>
           </div>
+        </div>
 
-          <div className="col text-center">
-            <div className="navbar-icon">
-              <BiListCheck className="navbar-icon-image" />
-              <span className="navbar-icon-name">Recipes</span>
-            </div>
+        <div className="text-center">
+          <div className="navbar-icon">
+            <BiListCheck className="navbar-icon-image" />
+            <span className="navbar-icon-name">Recipes</span>
           </div>
+        </div>
 
-          <div className="col text-center">
-            <div className="navbar-icon">
-              <BiBarChartAlt className="navbar-icon-image" />
-              <span className="navbar-icon-name">Shopping</span>
-            </div>
+        <div className="text-center">
+          <div className="navbar-icon">
+            <BiBarChartAlt className="navbar-icon-image" />
+            <span className="navbar-icon-name">Shopping</span>
           </div>
+        </div>
 
-          <div className="col text-center">
-            <div className="navbar-icon">
-              <BiBookOpen className="navbar-icon-image" />
-              <span className="navbar-icon-name">Breakdown</span>
-            </div>
+        <div className="text-center">
+          <div className="navbar-icon">
+            <BiBookOpen className="navbar-icon-image" />
+            <span className="navbar-icon-name">Breakdown</span>
           </div>
-
         </div>
       </div>
     </footer>
   );
-}
+};
 
 export default BottomNavbar;
-
